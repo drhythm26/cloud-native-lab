18c8a91b5664cd392a91fe9bf34b68517f3bd3b6
drhythm26 <drhythm26@gmail.com>
2026-06-24 14:10:04 +0800

    添加Prometheus监控：ServiceMonitor抓取API指标

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -22,3 +22,25 @@ echo ""
 
 echo "==> 获取 Argo CD UI 地址"
 kubectl get svc argocd-server -n argocd
+
+echo "==> 添加 Prometheus Helm 仓库"
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+echo "==> 安装 kube-prometheus-stack"
+kubectl create namespace monitoring --dry-run=client -o yaml | kubectl apply -f -
+
+helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
+-n monitoring \
+-f prometheus/values.yaml \
+--wait
+
+echo "==> 注册 ServiceMonitor"
+kubectl apply -f prometheus/servicemonitor.yaml
+
+echo "==> 获取 Grafana UI 地址和密码"
+kubectl get svc -n monitoring prometheus-grafana
+echo "Grafana 默认密码："
+kubectl get secret -n monitoring prometheus-grafana \
+-o jsonpath="{.data.admin-password}" | base64 -d
+echo ""
