dfe924ec0332a6c92557a045ec5b196b67263a7d
drhythm26 <drhythm26@gmail.com>
2026-06-30 15:54:49 +0800

    refactor(applications): applications 分支移除平台相关文件并将 Applicaion 与 chart 迁移至仓库根目录

--- infrastructure/gcp/apis.tf
+++
@@ -1,9 +0,0 @@
-resource "google_project_service" "compute" {
-  service            = "compute.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "container" {
-  service            = "container.googleapis.com"
-  disable_on_destroy = false
-}
